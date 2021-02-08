79c36a8a070c82d17b479650cb4a9899465feece
srmsk7 <76063758+srmsk7@users.noreply.github.com>
2021-02-08 19:51:30 +0700

    Create day_25_running_time_and_complexity.cpp

---
+++ Day 25 Running Time and Complexity/day_25_running_time_and_complexity.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(long n) {
+    
+    if (n == 1) {
+        return false;
+    }
+
+    for (int i = 2; i*i <= n; i++) {
+        if (n%i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    vector<bool> result;
+    long max = 0;
+    while (t--) {
+        long n;
+        cin >> n;
+        
+        result.push_back(isPrime(n));
+    }
+    for (int i = 0; i < result.size(); i++) {
+        cout << (result[i] ? "Prime" : "Not prime") << endl;
+    }
+    return 0;
+}
+
+/*
+12345
+*/
