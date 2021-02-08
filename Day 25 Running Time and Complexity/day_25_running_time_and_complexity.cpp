3ddbd91453540bfe1d657d049c2a4f264ba92eee
srmsk7 <76063758+srmsk7@users.noreply.github.com>
2021-02-08 19:52:04 +0700

    Update day_25_running_time_and_complexity.cpp

--- Day 25 Running Time and Complexity/day_25_running_time_and_complexity.cpp
+++ Day 25 Running Time and Complexity/day_25_running_time_and_complexity.cpp
@@ -2,17 +2,14 @@
 using namespace std;
 
 bool isPrime(long n) {
-    
     if (n == 1) {
         return false;
     }
-
     for (int i = 2; i*i <= n; i++) {
         if (n%i == 0) {
             return false;
         }
     }
-
     return true;
 }
 int main()
